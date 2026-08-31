f17e311a7614250b7314b3109bb26caaf41531a7
amoney79 <mackenziejoseph396@gmail.com>
2026-08-31 16:00:18 +0300

    Displaying on command line with different print statement

--- Arithmetic/Calculation.java
+++ Arithmetic/Calculation.java
@@ -5,5 +5,17 @@ public class Calculation {
         int x;
         x = ( 3 * 9 * (3 + ( 9 * 3 / (3 ) ) ) );
         System.out.println(x);
+
+        //one println
+        System.out.println("1 2 3 4");
+
+        // four println
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+
+        //one printf
+        System.out.printf("%d %d %d %d", 1, 2, 3, 4);
     }
 }
