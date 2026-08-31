41e22bc9545de82613bbdbe554f64a97f5f04966
amoney79 <mackenziejoseph396@gmail.com>
2026-08-31 17:09:54 +0300

    Modified shapes

--- DisplayingShapes.java
+++ DisplayingShapes.java
@@ -1,44 +1,56 @@
 public class DisplayingShapes {
-    
     public static void main(String[] args) {
-        System.out.println("*********");
-        System.out.println("*       *");
-        System.out.println("*       *");
-        System.out.println("*       *");
-        System.out.println("*********");
+        // Each array holds the lines of one shape
+        String[] square = {
+            "*********",
+            "*       *",
+            "*       *",
+            "*       *",
+            "*********"
+        };
 
+        String[] oval = {
+            "   ***   ",
+            "  *   *  ",
+            " *     * ",
+            " *     * ",
+            " *     * ",
+            "  *   *  ",
+            "   ***   "
+        };
 
-        System.out.println("   ***  ");
-        System.out.println("  *   * ");
-        System.out.println(" *     *");
-        System.out.println(" *     *");
-        System.out.println(" *     *");
-        System.out.println(" *     *");
-        System.out.println(" *     *");
-        System.out.println("  *   * ");
-        System.out.println("   ***  ");
+        String[] arrow = {
+            "   *   ",
+            "  ***  ",
+            " ***** ",
+            "*******",
+            "   *   ",
+            "   *   ",
+            "   *   ",
+            "   *   "
+        };
 
-        System.out.println("   *   ");
-        System.out.println("  ***  ");
-        System.out.println(" ***** ");
-        System.out.println("*******");
-        System.out.println("   *   ");
-        System.out.println("   *   ");
-        System.out.println("   *   ");
-        System.out.println("   *   ");
-        System.out.println("   *   ");
-        System.out.println("   *   ");
-        System.out.println("   *   ");
-        System.out.println("   *   ");
+        String[] diamond = {
+            "    *    ",
+            "  *   *  ",
+            " *     * ",
+            "*       *",
+            " *     * ",
+            "  *   *  ",
+            "    *    "
+        };
 
+        // Print them side by side
+        int maxLines = Math.max(Math.max(square.length, oval.length),
+                                Math.max(arrow.length, diamond.length));
 
-        System.out.println("    *   ");
-        System.out.println("  *   *  ");
-        System.out.println(" *     * ");
-        System.out.println("*       * ");
-        System.out.println(" *     * ");
-        System.out.println("  *   *  ");
-        System.out.println("    *   ");
+        for (int i = 0; i < maxLines; i++) {
+            String sq = (i < square.length) ? square[i] : "         ";
+            String ov = (i < oval.length) ? oval[i] : "         ";
+            String ar = (i < arrow.length) ? arrow[i] : "       ";
+            String di = (i < diamond.length) ? diamond[i] : "       ";
 
+            System.out.println(sq + "   " + ov + "   " + ar + "   " + di);
+        }
     }
 }
